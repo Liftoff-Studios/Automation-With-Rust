3ef4bb5a53999d2211ad34af197210bc38aeaf56
Liftoff Studios <abhijitbiswas07@gmail.com>
2021-12-27 14:06:10 +0530

    start

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use chrono::prelude::*;
+use std::process;
+
+
+fn main() {
+    open_browser();
+}
+
+fn time_in_range()->bool{
+
+    // This is the code for school start time
+    let utc: String = Utc::now().to_string();
+
+    let date1: Vec<&str> = utc.split(" ").collect();
+    let date2: Vec<&str> = date1[1].split(":").collect();
+
+    let time: i32 = date2[0].parse().expect("Error parsing");
+
+    let time = time + 5;
+
+    // This is the code for local time
+    let local: String = Local::now().to_string();
+
+    let local1: Vec<&str> = local.split(" ").collect();
+    let local2: Vec<&str> = local1[1].split(":").collect();
+
+    let time_local: i32 = local2[0].parse().expect("Error parsing");
+
+
+    // Checks if difference is 1 or -1 and decides accordingly
+    if time-time_local == 1 || time-time_local==(-1) ||time-time_local==0
+        {
+            return true;
+        }
+
+    false
+}
+
+fn open_browser(){
+    let time_checker: bool = time_in_range();
+    if time_checker{
+        let res = open::that("https://entrar.in/login/login");
+
+        if let Err(e) = res{
+            println!("Error opening browser: {}",e);
+        }
+
+        process::exit(1);
+    }
+
+    process::exit(1)
+}
